bf4a7377ffea6104733f648d771722afca7dc4cb
Leicsha Karen Saremo <120180371+ksaremo23@users.noreply.github.com>
2024-05-04 09:35:19 +0800

    1st commit

---
+++ src/components/App.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addContact, deleteContact, setFilter } from '../redux/actions';
+import { getContacts, getFilter } from '../redux/selectors';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+export const App = () => {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
+
+  const handleAddContact = newContact => {
+    // Placeholder for future Redux action
+     dispatch(addContact(newContact));
+  };
+
+  const handleDeleteContact = id => {
+    // Placeholder for future Redux action
+    dispatch(deleteContact(id));
+  };
+
+  const handleSetFilter = newFilter => {
+    dispatch(setFilter(newFilter));
+  };
+
+  // Calculate filtered contacts directly within the App component
+  const filteredContacts = contacts.filter(contact => 
+  contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={handleAddContact} contacts={contacts} />
+
+      <h2>Contacts</h2>
+      <Filter filter={filter} setfilter={handleSetFilter} />
+      <ContactList 
+        contacts={filteredContacts} //Passing the filteredContacts as props
+        deleteContact={handleDeleteContact}
+      />
+    </div>
+  );
+};
